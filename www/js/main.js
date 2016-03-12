c7288201da146070147aea169a73e3a1bc854620
nickheal <ns.heal@btinternet.com>
2016-03-12 23:23:31 +0000

    Added files via upload

--- www/js/main.js
+++ www/js/main.js
@@ -1,10 +1,28 @@
 var myApp = {
     init : function () {
+        myApp.loadPosts();
+        myApp.background();
+    },
+    loadPosts : function () {
+        $('body').append($('<div class="loader"></div>'));
+
         $.ajax({
             url: 'http://claws-for-thought.blogspot.co.uk/',
         }).done(function (data) {
             var posts = $(data).find('.mobile-post-outer');
             $('#testArea').html(posts);
+            $('.loader').addClass('finished');
+            setInterval(function () {
+                $('.mobile-post-outer').each(function () {
+                    $(this).addClass('loaded');
+                })
+            }, 1)
         })
+    },
+    background : function () {
+        var backgroundLoop = new TimelineMax({repeat:-1, yoyo:true, paused:true});
+        backgroundLoop.to('body', 5, {'background-color': 'rgb(210, 210, 210)'});
+
+        backgroundLoop.play();
     }
 }
